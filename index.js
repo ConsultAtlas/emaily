f4f0b0a8c1a0a1242e9999555a97bb58aeea613c
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2018-01-15 14:57:02 -0500

    started google oauth process

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'buddy'});
-});
+// Creates a new GoogleStrategy, tells passport we want to use it, 
+// and adds parameters on how we want to authenticate our users with google
+// GoogleStrategy() needs a client ID and a client secret (provided by Google).
 
-
+// clientID: 	161915244776-ta46c8iqeishu79qhem80m5qp129tb98.apps.googleusercontent.com
+// clientSecret: d3-PZIx781jFmSjrgzC0Z4rD
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
