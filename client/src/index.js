2031c32e8908d41194e6739937ce31cf4ba78d86
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2018-01-23 21:10:40 -0500

    added basic routes, always visible header, and materialize css with basic styling for header

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import 'materialize-css/dist/css/materialize.min.css' // webpack magic, importing css min files from our npm package
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
